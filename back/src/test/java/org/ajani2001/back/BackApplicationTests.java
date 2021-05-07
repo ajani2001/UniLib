673368879ae4a4867ce8c26e6afcc87bf5bd557b
Grigory Ivanov <ivanovgrigoriinikolaevich00@gmail.com>
2021-05-08 00:42:40 +0700

    finally initialized

---
+++ back/src/test/java/org/ajani2001/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ajani2001.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
